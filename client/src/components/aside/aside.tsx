b08d09da88f0ea81d8a26f5622facb747f9fefb7
Anas jaidi <anas.jaidi@icloud.com>
2023-12-03 03:44:05 +0100

    add: shred navbar and aside

--- client/src/components/aside/aside.tsx
+++ client/src/components/aside/aside.tsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import React from "react";
+import { TfiClose } from "react-icons/tfi";
+import { FaGripfire } from "react-icons/fa";
+
+import { IoSettingsOutline } from "react-icons/io5";
+import { GrFavorite } from "react-icons/gr";
+import { useAppDispatch } from "@/store";
+import { setAsideIsOpen } from "@/store/slices/ui.slice";
+import { useRouter } from "next/navigation";
+import { MdOutlineQuestionAnswer } from "react-icons/md";
+import { IoCreateOutline } from "react-icons/io5";
+
+
 
 export default function Aside() {
+  const dispatch = useAppDispatch();
+  const router = useRouter()
+
   return (
-    <div>Aside</div>
-  )
+    <div className="flex flex-col pt-4 px-5">
+      <button
+        className="flex justify-end  sm:hidden"
+        onClick={dispatch.bind(null, setAsideIsOpen(false))}
+      >
+        <TfiClose color="white" size="22" />
+      </button>
+      <div className="mx-auto mt-2">
+        <h1 className="text-2xl text-white font-thin">Logo.io</h1>
+      </div>
+      <div className="mt-16 flex flex-col gap-12 justify-center">
+        {[
+          {icon: MdOutlineQuestionAnswer, label: 'Questions', href: 'questions'},
+          { icon: FaGripfire, label: "Trending", href: 'trending' },
+          { icon: IoSettingsOutline, label: "Settings", href: 'settings' },
+          { icon: GrFavorite, label: "Favorites", href: 'liked' },
+          { icon: IoCreateOutline, label: "Cretaed", href: 'personal' },
+        ].map((btn, i) => (
+          <button
+            className="flex justify-center md:justify-start text-white gap-2 text-xl font-light"
+            key={i}
+            onClick={router.push.bind(null, btn.href)}
+          >
+            <btn.icon color="white" size="22" />
+            <h2>{btn.label}</h2>
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 }
--- client/src/components/navbar/navbar.tsx
+++ client/src/components/navbar/navbar.tsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React from "react";
+import { CiMenuBurger } from "react-icons/ci";
+import { useAppDispatch } from "@/store";
+import { setAnswering, setAsideIsOpen } from "@/store/slices/ui.slice";
+import Image from "next/image";
 
 export default function Navbar() {
+  const dispatch = useAppDispatch();
+
+  const onAddClicked = () => {
+    dispatch(setAnswering(true));
+  };
+
   return (
-    <div>Navbar</div>
-  )
+    <div className="text-white font-light px-6 sm:px-12 flex gap-4 items-center h-full justify-between">
+      <div className=" flex items-center gap-6">
+        <button
+          className="sm:hidden"
+          onClick={dispatch.bind(null, setAsideIsOpen(true))}
+        >
+          <CiMenuBurger color="white" size="28" />
+        </button>
+        <button
+          className="bg-[#DDE6ED] px-4 sm:px-5 text-sm py-2 sm:py-3 rounded-lg overflow-hidden text-[#27374D]"
+          style={{ borderRadius: "2px" }}
+          onClick={onAddClicked}
+        >
+          Add Q&A
+        </button>
+      </div>
+      <div className="flex gap-3 items-center">
+        <Image
+          src={"/profile.jpeg"}
+          alt=""
+          width={38}
+          height={38}
+          className="rounded-full"
+        />
+        <p className="font-light">Hi, Robin</p>
+      </div>
+    </div>
+  );
 }
